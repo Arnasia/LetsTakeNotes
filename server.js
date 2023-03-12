3302c84fa6ba8492e367ecf14bfc4bfcc2b69880
Arnasia Jackson <arnasial.jackson@gmail.com>
2023-03-12 11:10:06 -0400

    create a function to take note and push out response

--- server.js
+++ server.js
@@ -6,8 +6,21 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+//parses incoming JSON data
 app.use(express.urlencoded({extended: true}));
+
+//parses incoming array data
 app.use(express.json());
+
+//make the files static
 app.use(express.static('public'));
+
+
+//write a new note
+function takeNote (body) {
+    const note = body;
+    LetsTakeNotes.push(note);
+    
+}
 
 
