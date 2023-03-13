01aae3bca92e638fb0d35fe535831bb89e44564f
Arnasia Jackson <arnasial.jackson@gmail.com>
2023-03-12 21:27:56 -0400

    app is getting notes and posting no delete yet

--- server.js
+++ server.js
@@ -3,57 +3,56 @@ const fs = require ('fs');
 const express = require ("express");
 const path = require("path")
 const app = express();
-const uuid = require('uuid')
-
 const PORT = process.env.PORT || 3001;
 
-const { LetsTakeNotes } = require ("./db/db")
+//generate random id
+const uuid = require('uuid')
+
+const { notes } = require ("./db/db")
 
 //parses incoming JSON data
 app.use(express.urlencoded({extended: true}));
-
 //parses incoming array data
 app.use(express.json());
-
 //make the files static
 app.use(express.static('public'));
 
 
 //write a new note
-function takeNote (body) {
-    const note = body;
-    LetsTakeNotes.push(note);
+function takeNote (body, notesArray) {
+    const note = body
+    notesArray.push(note);
     fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify({ LetsTakeNotes }, null, 2)
+        path.join(__dirname, "./db/db.json"),
+        JSON.stringify({ notes: notesArray }, null, 2)
     );
     return note;
 };
 
 //Routes
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  req.body.id = uuid.v4();
+  const note = takeNote(req.body, notes);
+  res.json(note);
+});
+
+// POST
+// readFile
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.get('/LetsTakeNotes', (req, res) => {
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
-  });
-  
-  app.get('/api/LetsTakeNotes', (req, res) => {
-    res.json(LetsTakeNotes);
-  });
+});
 
-  app.post('/api/notes', (req, res) => {
-    req.body.id = uuid();
-    const note = takeNote(req.body, LetsTakeNotes);
-    res.json(note);
-  });
-
+  //app listerner
   app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
   });
-  
-
-  
-
-
